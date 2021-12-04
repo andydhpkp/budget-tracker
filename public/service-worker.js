bd54482a767c03cb769f43cfe206184bf70d1d29
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-04 13:13:35 -0700

    added manifest and changes to html, uninstalled and re-installed tsc because of manifest errors with semi-colon

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,7 +22,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('fetch', function(e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function(request) {
+        caches.match(e.request).then(request => {
             if (request) {
                 console.log('responding with cache : ' + e.request.url)
                 return fetch(e.request)
@@ -34,7 +34,7 @@ self.addEventListener('fetch', function(e) {
 //cache the resources
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then(cache => {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
@@ -44,7 +44,7 @@ self.addEventListener('install', function(e) {
 //delete unused caches
 self.addEventListener('activate', function(e) {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
+        caches.keys().then(keyList => {
             let cacheKeepList = keyList.filter(function(key) {
                 return key.indexOf(APP_PREFIX)
             })
